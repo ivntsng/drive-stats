c5cfd79f5602fdcabc485a68ff3b6321d40ea177
Ivan <45982459+ivntsng@users.noreply.github.com>
2024-04-11 09:55:12 -0700

    Feature/frontend (#17)
    * Updated toast UI
    
    * added user authentication with UserContext
    
    * code cleanup - removed comments
    
    * code cleanup - removed comments
    
    * Worked on Login and Signup Form UI and functions
    
    * Fixed issues with exiting the UI when Login or Signup form is open
    
    * Worked on NavBar to include an option to view My Garage
    
    * Additional changes
    
    * Merge conflict changes
    
    * Fixed issue when upon login via the navbar login form will not close.
    
    * Feature/frontend (#15)
    
    * Updated toast UI
    
    * added user authentication with UserContext
    
    * code cleanup - removed comments
    
    * code cleanup - removed comments
    
    * Worked on Login and Signup Form UI and functions
    
    * Fixed issues with exiting the UI when Login or Signup form is open
    
    * Worked on NavBar to include an option to view My Garage
    
    * Additional changes
    
    * Merge conflict changes
    
    ---------
    
    Co-authored-by: casper <ivntsng@gmail.com>
    
    * Started create_vehicle form
    
    * Fixed merge conflicts
    
    * Updated changes
    
    * Fixed import issues
    
    ---------
    
    Co-authored-by: casper <ivntsng@gmail.com>

--- drive-stats/src/NavBar.jsx
+++ drive-stats/src/NavBar.jsx
@@ -5,6 +5,15 @@ import { useTheme } from '@/components/theme-provider'
 import { handleLogout } from './components/login'
 import { useToast } from '@/components/ui/use-toast'
 import { UserContext } from './UserContext'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     const { setTheme } = useTheme()
@@ -26,17 +35,90 @@ function NavBar() {
 
     return (
         <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
-            <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                <a href="#" className="flex items-center">
-                    <img
-                        src="https://www.svgrepo.com/show/471147/car-01.svg"
-                        className="h-6 mr-3 sm:h-9"
-                        alt="DriveStats Logo"
-                    />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                        DriveStats
-                    </span>
-                </a>
+            <div className="flex items-center justify-between max-w-screen-xl px-4 mx-auto">
+                <div className="flex items-center">
+                    <a href="/" className="flex items-center">
+                        <img
+                            src="https://www.svgrepo.com/show/471147/car-01.svg"
+                            className="h-6 mr-3 sm:h-9"
+                            alt="DriveStats Logo"
+                        />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            DriveStats
+                        </span>
+                    </a>
+                </div>
+                <div className="flex items-center justify-center flex-grow">
+                    {user && (
+                        <NavigationMenu>
+                            <NavigationMenuList>
+                                <NavigationMenuItem>
+                                    <NavigationMenuTrigger>
+                                        My Garage
+                                    </NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                                            <li className="row-span-3">
+                                                <NavigationMenuLink asChild>
+                                                    <a
+                                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                                        href="/"
+                                                    >
+                                                        {/* Include Icons here if needed */}
+                                                        <div className="mb-2 mt-4 text-lg font-medium">
+                                                            shadcn/ui
+                                                        </div>
+                                                        <p className="text-sm leading-tight text-muted-foreground">
+                                                            Beautifully designed
+                                                            components built
+                                                            with Radix UI and
+                                                            Tailwind CSS.
+                                                        </p>
+                                                    </a>
+                                                </NavigationMenuLink>
+                                            </li>
+                                            {/* Include other documentation links here */}
+                                            <li>
+                                                <a
+                                                    href="/addvehicle"
+                                                    className="text-blue-500"
+                                                >
+                                                    Register Vehicle
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a
+                                                    href="/docs/installation"
+                                                    className="text-blue-500"
+                                                >
+                                                    Add Maintenance Log
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a
+                                                    href="/docs/primitives/typography"
+                                                    className="text-blue-500"
+                                                >
+                                                    Typography
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                                {/* Include other navigation menu items here */}
+                                <NavigationMenuItem>
+                                    <NavigationMenuContent>
+                                        <NavigationMenuList>
+                                            <NavigationMenuItem>
+                                                <Link></Link>
+                                            </NavigationMenuItem>
+                                        </NavigationMenuList>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    )}
+                </div>
                 <div className="flex items-center lg:order-2">
                     {/* Rest of your code for user actions */}
                     {user ? (
