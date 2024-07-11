0e06e489d44cf40c84ba6ea2ad38436a806412ac
casper <ivntsng@gmail.com>
2024-07-10 20:12:29 -0700

    Added token storage upon login, added auth to pages

--- drive-stats/src/App.jsx
+++ drive-stats/src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from './MainPage'
 import CreateVehicle from './components/vehicles/CreateVehicle'
@@ -6,10 +7,10 @@ import VehicleStat from './components/vehicle_stats/CreateStatLog'
 import CreateBugReportForm from './components/bug_reports/create_bug_reports'
 import NavBar from './NavBar'
 import { ThemeProvider } from '@/components/theme-provider'
-import React, { useState, useEffect } from 'react'
 import { UserContext } from './UserContext'
 import Garage from './components/vehicles/Garage'
 import { TimerProvider } from './components/timer'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const API_HOST = import.meta.env.VITE_API_HOST
 
@@ -18,15 +19,22 @@ if (!API_HOST) {
 }
 
 function App() {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(() => {
+        const token = sessionStorage.getItem('token')
+        const username = sessionStorage.getItem('username')
+        if (token && username) {
+            return { token, username }
+        }
+        return null
+    })
 
     useEffect(() => {
         const token = sessionStorage.getItem('token')
         const username = sessionStorage.getItem('username')
-        if (token && username) {
+        if (token && username && !user) {
             setUser({ token, username })
         }
-    }, [])
+    }, [user])
 
     const value = { user, setUser }
     return (
@@ -40,23 +48,41 @@ function App() {
                                 <Route path="/" element={<MainPage />} />
                                 <Route
                                     path="/vehicles/register"
-                                    element={<CreateVehicle />}
+                                    element={
+                                        <ProtectedRoute
+                                            element={<CreateVehicle />}
+                                        />
+                                    }
                                 />
                                 <Route
                                     path="/vehicles/garage"
-                                    element={<Garage />}
+                                    element={
+                                        <ProtectedRoute element={<Garage />} />
+                                    }
                                 />
                                 <Route
                                     path="/vehicles/garage/:vehicle_id"
-                                    element={<VehicleDetail />}
+                                    element={
+                                        <ProtectedRoute
+                                            element={<VehicleDetail />}
+                                        />
+                                    }
                                 />
                                 <Route
                                     path="/vehicles/garage/maintenance-log/"
-                                    element={<VehicleStat />}
+                                    element={
+                                        <ProtectedRoute
+                                            element={<VehicleStat />}
+                                        />
+                                    }
                                 />
                                 <Route
                                     path="/bug-report/create"
-                                    element={<CreateBugReportForm />}
+                                    element={
+                                        <ProtectedRoute
+                                            element={<CreateBugReportForm />}
+                                        />
+                                    }
                                 />
                             </Routes>
                         </div>
